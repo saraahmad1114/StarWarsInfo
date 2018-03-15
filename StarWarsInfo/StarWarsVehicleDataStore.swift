0aa69a9ba31a9a632a1a818ade0658b353839620
saraahmad1114 <asara1114@gmail.com>
2018-03-14 20:58:03 -0400

    added StarWarsVehicleDataStore file

---
+++ StarWarsInfo/StarWarsVehicleDataStore.swift
@@ -0,0 +1,11 @@
+//
+//  StarWarsVehicleDataStore.swift
+//  StarWarsInfo
+//
+//  Created by Sara Ahmad on 3/14/18.
+//  Copyright © 2018 Sara Ahmad. All rights reserved.
+//
+
+import Foundation
+
+
