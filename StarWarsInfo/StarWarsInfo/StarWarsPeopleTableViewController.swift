2baed1481b9a32828383eb66192bb1b838f10392
saraahmad1114 <asara1114@gmail.com>
2018-03-15 17:49:07 -0400

    chnages

--- StarWarsInfo/StarWarsInfo/StarWarsPeopleTableViewController.swift
+++ StarWarsInfo/StarWarsInfo/StarWarsPeopleTableViewController.swift
@@ -9,80 +9,64 @@
 import UIKit
 
 class StarWarsPeopleTableViewController: UITableViewController {
-
+    
+    let store = StarWarsPeopleDataStore.sharedInstance
+    var page = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.backgroundColor = UIColor.black
+        self.store.getStarWarsPeopleInformation(page: self.store.page) { (starwarsPeopleArray) in
+            OperationQueue.main.addOperation {
+                self.tableView.reloadData()
+            }
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return self.store.starWarsPeopleArray.count
     }
-
-    /*
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath)
+        cell.textLabel?.textColor = UIColor.white
+        cell.backgroundColor = UIColor.black
+        cell.textLabel?.text = self.store.starWarsPeopleArray[indexPath.row].name
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let lastElement = self.store.starWarsPeopleArray.count-1
+        if indexPath.row == lastElement{
+            loadMoreData()
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func loadMoreData(){
+        self.store.page = self.store.page + 1
+        self.store.getStarWarsPeopleInformation(page: self.store.page) { (newArray) in
+            OperationQueue.main.addOperation({
+                self.tableView.reloadData()
+            })
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "peopleDetailSegue"{
+//            if let destinationVC = segue.destination as? PeopleDetailViewController {
+//                let neededIndexPath = self.tableView.indexPathForSelectedRow!
+//                destinationVC.starWarsPeopleObject = self.store.starWarsPeopleArray[neededIndexPath.row]
+//            }
+//        }
+//    }
+    
 }
