ee33d3db4d7e8852041ec86c86bd7154d99af059
saraahmad1114 <asara1114@gmail.com>
2018-03-14 20:59:29 -0400

    added StarWarsFilmsDataStore

---
+++ StarWarsInfo/StarWarsInfo/StarWarsDatastore/StarWarsFilmsDataStore.swift
@@ -0,0 +1,33 @@
+//
+//  StarWarsFilmsDataStore.swift
+//  StarWarsInfo
+//
+//  Created by Sara Ahmad on 3/14/18.
+//  Copyright © 2018 Sara Ahmad. All rights reserved.
+//
+
+import Foundation
+
+class StarWarsFilmsDataStore {
+    
+    static let sharedInstance = StarWarsFilmsDataStore()
+    private init() {}
+    
+    var starWarsFilmsArray = [StarWarsFilms]()
+    
+    func getStarWarsFilms ( completion:@escaping ([StarWarsFilms]) -> ()){
+        
+        StarWarsFilmsAPIClient.getStarWarsFilmsInformation { (fiilmsArray) in
+            
+            for singleDictionary in fiilmsArray{
+                
+                guard let unwrappedSingleDictionary = singleDictionary as? [String: Any] else{print("singleDictionary did not unwrap"); return}
+                
+                var starWarsFilmObject = StarWarsFilms.init(jsonDictionary: unwrappedSingleDictionary)
+                
+                self.starWarsFilmsArray.append(starWarsFilmObject)
+            }
+            completion(self.starWarsFilmsArray)
+        }
+    }
+}
