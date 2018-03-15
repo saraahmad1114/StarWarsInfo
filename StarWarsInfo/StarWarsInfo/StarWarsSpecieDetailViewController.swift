154bdd1c2efa18a47afb84a7b2a058e73aa8bcea
saraahmad1114 <asara1114@gmail.com>
2018-03-15 19:57:47 -0400

    changes

---
+++ StarWarsInfo/StarWarsInfo/StarWarsSpecieDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  StarWarsSpecieDetailViewController.swift
+//  StarWarsInfo
+//
+//  Created by Sara Ahmad on 3/15/18.
+//  Copyright © 2018 Sara Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class StarWarsSpecieDetailViewController: UIViewController {
+    
+    var specieObject: StarWarsSpecies?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
