6ddd386943fd6c85bfb6ae00cdbcd8d22eb34d81
saraahmad1114 <asara1114@gmail.com>
2018-03-14 20:58:21 -0400

    filled file

--- StarWarsInfo/StarWarsVehicleDataStore.swift
+++ StarWarsInfo/StarWarsVehicleDataStore.swift
@@ -8,4 +8,34 @@
 
 import Foundation
 
-
+class StarWarsVehicleDataStore{
+    
+    static let sharedInstance = StarWarsVehicleDataStore()
+    private init() {}
+    var starWarsVehicleArray = [StarWarsVehicles]()
+    var page = 1
+    
+    func getStarWarsVehicles(page: Int, completion:@escaping ([StarWarsVehicles]) -> ()){
+        
+        StarWarsVehicleAPIClient.getStarWarsVehicleInformation(page: page) { (jsonArray) in
+            
+            for singleDictionary in jsonArray{
+                
+                guard let unwrappedSingleDictionary = singleDictionary as? [String: Any] else{print("singleDictionary did not unwrap"); return}
+                
+                var starWarsVehicleObject = StarWarsVehicles.init(jsonDictionary: unwrappedSingleDictionary)
+                
+                self.starWarsVehicleArray.append(starWarsVehicleObject)
+                
+            }
+            completion(self.starWarsVehicleArray)
+        }
+        
+        
+    }
+    
+    func retrieveNextPageInformation()
+    {
+        self.page += 1
+    }
+}
