ec81687b304758d4b361c5a0e0fe5e90851a8211
saraahmad1114 <asara1114@gmail.com>
2018-03-27 20:25:15 -0400

    chnages

--- StarWarsInfo/StarWarsInfo/StarWar Objects/StarWarsSpecies.swift
+++ StarWarsInfo/StarWarsInfo/StarWar Objects/StarWarsSpecies.swift
@@ -27,27 +27,4 @@ struct StarWarsSpecies: Decodable{
     var average_lifespan: String?
     var language: String?
     
-//    init(jsonDictionary: [String: Any]) {
-//        guard
-//            let name = jsonDictionary["name"] as? String,
-//            let classification = jsonDictionary["classification"] as? String,
-//            let designation = jsonDictionary["designation"] as? String,
-//            let averageHeight = jsonDictionary["average_height"] as? String,
-//            let skinColor = jsonDictionary["skin_colors"] as? String,
-//            let hairColor = jsonDictionary["hair_colors"] as? String,
-//            let eyeColor = jsonDictionary["eye_colors"] as? String,
-//            let lifespan = jsonDictionary["average_lifespan"] as? String,
-//            let language = jsonDictionary["language"] as? String
-//            else{print("did not unwrap"); return}
-//
-//        self.name = name
-//        self.classification = classification
-//        self.designation = designation
-//        self.averageHeight = averageHeight
-//        self.skinColor = skinColor
-//        self.hairColor = hairColor
-//        self.eyeColor = eyeColor
-//        self.lifespan = lifespan
-//        self.language = language
-//    }
 }
