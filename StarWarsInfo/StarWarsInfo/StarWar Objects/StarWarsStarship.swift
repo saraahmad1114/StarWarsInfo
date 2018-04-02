94c7e49cb5e94af8c40e0847f6a4d33c893ff5f6
saraahmad1114 <asara1114@gmail.com>
2018-04-02 17:36:11 -0400

    redefined starwarsstarship class

--- StarWarsInfo/StarWarsInfo/StarWar Objects/StarWarsStarship.swift
+++ StarWarsInfo/StarWarsInfo/StarWar Objects/StarWarsStarship.swift
@@ -8,8 +8,17 @@
 
 import Foundation
 
-class StarWarsStarship{
+class MainStarWarsStarshipJson: Decodable{
     
+    var count: Int
+    var next: String?
+    var previous: String?
+    var results: [StarWarsStarship]
+    
+}
+
+struct StarWarsStarship: Decodable{
+
     var name: String?
     var model: String?
     var manufacturer: String?
@@ -19,30 +28,31 @@ class StarWarsStarship{
     var crew: String?
     var cargoCapacity: String?
     var starshipClass: String?
-    
-    init(jsonDictionary: [String: Any]) {
-        guard
-            let name = jsonDictionary["name"] as? String,
-            let model = jsonDictionary["model"] as? String,
-            let manufacturer = jsonDictionary["manufacturer"] as? String,
-            let costInCredits = jsonDictionary["cost_in_credits"] as? String,
-            let length = jsonDictionary["length"] as? String,
-            let maxAtmosphericSpeed = jsonDictionary["max_atmosphering_speed"] as? String,
-            let crew = jsonDictionary["crew"] as? String,
-            let cargoCapacity = jsonDictionary["cargo_capacity"] as? String,
-            let starShipClass = jsonDictionary["starship_class"] as? String
-            
-            else {print("did not unwrap"); return}
-        
-        self.name = name
-        self.model = model
-        self.manufacturer = manufacturer
-        self.costInCredits = costInCredits
-        self.length = length
-        self.maxAtmosphericSpeed = maxAtmosphericSpeed
-        self.crew = crew
-        self.cargoCapacity = cargoCapacity
-        self.starshipClass = starShipClass
-    }
-    
 }
+//
+//    init(jsonDictionary: [String: Any]) {
+//        guard
+//            let name = jsonDictionary["name"] as? String,
+//            let model = jsonDictionary["model"] as? String,
+//            let manufacturer = jsonDictionary["manufacturer"] as? String,
+//            let costInCredits = jsonDictionary["cost_in_credits"] as? String,
+//            let length = jsonDictionary["length"] as? String,
+//            let maxAtmosphericSpeed = jsonDictionary["max_atmosphering_speed"] as? String,
+//            let crew = jsonDictionary["crew"] as? String,
+//            let cargoCapacity = jsonDictionary["cargo_capacity"] as? String,
+//            let starShipClass = jsonDictionary["starship_class"] as? String
+//
+//            else {print("did not unwrap"); return}
+//
+//        self.name = name
+//        self.model = model
+//        self.manufacturer = manufacturer
+//        self.costInCredits = costInCredits
+//        self.length = length
+//        self.maxAtmosphericSpeed = maxAtmosphericSpeed
+//        self.crew = crew
+//        self.cargoCapacity = cargoCapacity
+//        self.starshipClass = starShipClass
+//    }
+//
+//}
