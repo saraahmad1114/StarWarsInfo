f87d6739065015531be24873b1f192ef71e3b263
saraahmad1114 <asara1114@gmail.com>
2018-04-02 17:47:07 -0400

    changes

--- StarWarsInfo/StarWarsInfo/StarWar Objects/StarWarsStarship.swift
+++ StarWarsInfo/StarWarsInfo/StarWar Objects/StarWarsStarship.swift
@@ -14,7 +14,6 @@ class MainStarWarsStarshipJson: Decodable{
     var next: String?
     var previous: String?
     var results: [StarWarsStarship]
-    
 }
 
 struct StarWarsStarship: Decodable{
@@ -30,29 +29,3 @@ struct StarWarsStarship: Decodable{
     var starshipClass: String?
 }
 
-//    init(jsonDictionary: [String: Any]) {
-//        guard
-//            let name = jsonDictionary["name"] as? String,
-//            let model = jsonDictionary["model"] as? String,
-//            let manufacturer = jsonDictionary["manufacturer"] as? String,
-//            let costInCredits = jsonDictionary["cost_in_credits"] as? String,
-//            let length = jsonDictionary["length"] as? String,
-//            let maxAtmosphericSpeed = jsonDictionary["max_atmosphering_speed"] as? String,
-//            let crew = jsonDictionary["crew"] as? String,
-//            let cargoCapacity = jsonDictionary["cargo_capacity"] as? String,
-//            let starShipClass = jsonDictionary["starship_class"] as? String
-//
-//            else {print("did not unwrap"); return}
-//
-//        self.name = name
-//        self.model = model
-//        self.manufacturer = manufacturer
-//        self.costInCredits = costInCredits
-//        self.length = length
-//        self.maxAtmosphericSpeed = maxAtmosphericSpeed
-//        self.crew = crew
-//        self.cargoCapacity = cargoCapacity
-//        self.starshipClass = starShipClass
-//    }
-//
-//}
