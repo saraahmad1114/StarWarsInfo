fa834d00d729c50175f51452f9562b355e0f411a
saraahmad1114 <asara1114@gmail.com>
2018-04-06 18:52:15 -0400

    updating labelg

--- StarWarsInfo/StarWarsInfo/DetailViewControllers/StarWarsStarshipDetailViewController.swift
+++ StarWarsInfo/StarWarsInfo/DetailViewControllers/StarWarsStarshipDetailViewController.swift
@@ -11,12 +11,25 @@ import UIKit
 class StarWarsStarshipDetailViewController: UIViewController {
     
     var starShipObject: StarWarsStarship?
-
+    
+    @IBOutlet weak var nameUpdateLabel: UILabel!
+    @IBOutlet weak var modelUpdateLabel: UILabel!
+    @IBOutlet weak var manufacturerUpdateLabel: UILabel!
+    @IBOutlet weak var costUpdateLabel: UILabel!
+    @IBOutlet weak var lengthUpdateLabel: UILabel!
+    @IBOutlet weak var speedUpdateLabel: UILabel!
+    @IBOutlet weak var crewUpdateLabel: UILabel!
+    @IBOutlet weak var cargoUpdateLabel: UILabel!
+    @IBOutlet weak var starShipUpdateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         guard let starShipObj = self.starShipObject else {print("did not unwrap starshipObject"); return}
         
+        if let name = starShipObj.name{
+            self.nameUpdateLabel.text = name 
+        }
         
         
     }
