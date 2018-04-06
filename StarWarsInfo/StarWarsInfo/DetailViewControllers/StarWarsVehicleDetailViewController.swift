54927f8c58d6e24292b51fed002b3734e88293a9
saraahmad1114 <asara1114@gmail.com>
2018-04-06 19:07:12 -0400

    created outlets for labels

--- StarWarsInfo/StarWarsInfo/DetailViewControllers/StarWarsVehicleDetailViewController.swift
+++ StarWarsInfo/StarWarsInfo/DetailViewControllers/StarWarsVehicleDetailViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class StarWarsVehicleDetailViewController: UIViewController {
     
-     var vehicleObj: StarWarsVehicle?
-
+    var vehicleObj: StarWarsVehicle?
+    
+    @IBOutlet weak var nameUpdateLabel: UILabel!
+    @IBOutlet weak var modelUpdateLabel: UILabel!
+    @IBOutlet weak var manufacturerUpdateLabel: UILabel!
+    @IBOutlet weak var costUpdateLabel: UILabel!
+    @IBOutlet weak var maxSpeedUpdate: UILabel!
+    @IBOutlet weak var crewUpdateLabel: UILabel!
+    @IBOutlet weak var vehicleClassUpdateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
