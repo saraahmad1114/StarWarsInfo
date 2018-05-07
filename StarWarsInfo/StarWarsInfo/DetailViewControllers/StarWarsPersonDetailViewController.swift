35b58be07f953adf60930209c8b457ceb1855733
saraahmad1114 <asara1114@gmail.com>
2018-05-06 22:37:57 -0400

    updating peopledetail vc

--- StarWarsInfo/StarWarsInfo/DetailViewControllers/StarWarsPersonDetailViewController.swift
+++ StarWarsInfo/StarWarsInfo/DetailViewControllers/StarWarsPersonDetailViewController.swift
@@ -26,30 +26,30 @@ class StarWarsPersonDetailViewController: UIViewController {
 
         guard let starwarsObject = self.starWarsPeopleObject else {print("did not unwrap the first star wars object"); return}
         
-        if let name = starwarsObject.name {
-           self.nameUpdateLabel.text = name
-        }
-        if let height = starwarsObject.height{
-           self.heightUpdateLabel.text = height
-        }
-        if let weight = starwarsObject.weight{
-           self.weightUpdateLabel.text = weight
-        }
-        if let hairColor = starwarsObject.hair_color{
-           self.hairColorUpdateLabel.text = hairColor
-        }
-        if let skinColor = starwarsObject.skin_color{
-           self.skinColorUpdateLabel.text = skinColor
-        }
-        if let eyeColor = starwarsObject.eye_color{
-           self.eyeColorUpdateLabel.text = eyeColor
-        }
-        if let birthYear = starwarsObject.birth_year{
-            self.birthYearUpdateLabel.text = birthYear
-        }
-        if let gender = starwarsObject.gender{
-            self.genderUpdateLabel.text = gender
-        }
+//        if let name = starwarsObject.name {
+//           self.nameUpdateLabel.text = name
+//        }
+//        if let height = starwarsObject.height{
+//           self.heightUpdateLabel.text = height
+//        }
+//        if let weight = starwarsObject.weight{
+//           self.weightUpdateLabel.text = weight
+//        }
+//        if let hairColor = starwarsObject.hair_color{
+//           self.hairColorUpdateLabel.text = hairColor
+//        }
+//        if let skinColor = starwarsObject.skin_color{
+//           self.skinColorUpdateLabel.text = skinColor
+//        }
+//        if let eyeColor = starwarsObject.eye_color{
+//           self.eyeColorUpdateLabel.text = eyeColor
+//        }
+//        if let birthYear = starwarsObject.birth_year{
+//            self.birthYearUpdateLabel.text = birthYear
+//        }
+//        if let gender = starwarsObject.gender{
+//            self.genderUpdateLabel.text = gender
+//        }
     }
 
     override func didReceiveMemoryWarning() {
