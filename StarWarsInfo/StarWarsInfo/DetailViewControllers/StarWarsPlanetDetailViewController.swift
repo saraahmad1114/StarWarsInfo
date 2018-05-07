6e51eefe1083ad0d4bc464d273c6c1dfff4139f3
saraahmad1114 <asara1114@gmail.com>
2018-05-06 21:09:38 -0400

    aligned the name label

--- StarWarsInfo/StarWarsInfo/DetailViewControllers/StarWarsPlanetDetailViewController.swift
+++ StarWarsInfo/StarWarsInfo/DetailViewControllers/StarWarsPlanetDetailViewController.swift
@@ -22,26 +22,26 @@ class StarWarsPlanetDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let starWarObject = self.starWarsPlanetObject else {print("did not unwrap the upper level objecct"); return}
+        //guard let starWarObject = self.starWarsPlanetObject else {print("did not unwrap the upper level objecct"); return}
         
-        if let name = starWarObject.name {
-            self.upateNameLabel.text = name
-        }
-        if let rotation = starWarObject.rotation_period{
-            self.updateRotationLabel.text = rotation
-        }
-        if let orbital = starWarObject.orbital_period{
-            self.updateOrbital.text = orbital
-        }
-        if let diameter = starWarObject.diameter{
-            self.updateDiameter.text = diameter
-        }
-        if let gravity = starWarObject.gravity{
-            self.updateGravity.text = gravity
-        }
-        if let population = starWarObject.population{
-            self.updatePopulation.text = population
-        }
+        //if let name = starWarObject.name {
+            //self.upateNameLabel.text = name
+        //}
+        //if let rotation = starWarObject.rotation_period{
+            //self.updateRotationLabel.text = rotation
+        //}
+        //if let orbital = starWarObject.orbital_period{
+            //self.updateOrbital.text = orbital
+        //}
+        //if let diameter = starWarObject.diameter{
+            //self.updateDiameter.text = diameter
+        //}
+        //if let gravity = starWarObject.gravity{
+            //self.updateGravity.text = gravity
+        //}
+        //if let population = starWarObject.population{
+            //self.updatePopulation.text = population
+        //}
 
     }
 
